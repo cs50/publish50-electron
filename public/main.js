a842b23a9d40ab60a8e5e236f97b119100794e09
Kareem Zidane <kzidane@cs50.harvard.edu>
2019-02-14 10:37:58 -0500

    require prefs after ready

--- public/main.js
+++ public/main.js
@@ -41,7 +41,7 @@ const defaults = {
   }
 }
 
-let prefs = loadSettings()
+let prefs
 
 function initialize() {
   mainWindow = new BrowserWindow({
@@ -117,228 +117,234 @@ function sendToMainWindow(event, data) {
   mainWindow.send(event, data)
 }
 
-Object.values(queues).forEach((queue) => {
-  queue.on('error', (err) => {
-    console.error(err)
+
+
+app.on('ready', () => {
+  prefs = loadSettings()
+  initialize()
+
+
+  Object.values(queues).forEach((queue) => {
+    queue.on('error', (err) => {
+      console.error(err)
+    })
+
+    queue.on('waiting', async (jobId) => {
+      try {
+        const job = await queue.getJob(jobId)
+        sendToMainWindow('job pending', { job })
+      }
+      catch (err) {
+        dialog.showErrorBox('Failed to get job', err.toString())
+      }
+    })
+
+    queue.on('active', (job, jobPromise) => {
+      sendToMainWindow('job started', { job })
+    })
+
+    queue.on('stalled', (job) => {
+      sendToMainWindow('job stalled', { job })
+    })
+
+    queue.on('progress', (job, progress) => {
+      sendToMainWindow('job progress', { job, progress })
+    })
+
+    queue.on('completed', (job, result) => {
+      sendToMainWindow('job succeeded', { job, result })
+    })
+
+    queue.on('failed', (job, err) => {
+      sendToMainWindow('job failed', { job, err })
+    })
   })
 
-  queue.on('waiting', async (jobId) => {
+  ipc.on('get preferences', (event, data) => {
+    event.returnValue = loadSettings()
+  })
+
+  ipc.on('save preferences', (event, data) => {
+    prefs = validateSettings(data)
+    settings.setAll(prefs)
+  })
+
+  ipc.on('reset preferences', (event, data) => {
+    settings.setAll(defaults)
+    event.returnValue = defaults
+  })
+
+  ipc.on('get job', async (event, data) => {
+    const queueName = data.job.queue.name
     try {
-      const job = await queue.getJob(jobId)
-      sendToMainWindow('job pending', { job })
+      event.returnValue = await queues[queueName].getJob(data.job.id)
     }
     catch (err) {
       dialog.showErrorBox('Failed to get job', err.toString())
     }
   })
 
-  queue.on('active', (job, jobPromise) => {
-    sendToMainWindow('job started', { job })
+  ipc.on('abort job', async (event, data) => {
+    Object.values(queues[data.job.queue.name].childPool.retained).forEach((child) => {
+      child.send({__abortJobId__: data.job.id})
+    })
   })
 
-  queue.on('stalled', (job) => {
-    sendToMainWindow('job stalled', { job })
-  })
+  ipc.on('get finished jobs', async (event, data) => {
+    let completedJobs, failedJobs
 
-  queue.on('progress', (job, progress) => {
-    sendToMainWindow('job progress', { job, progress })
-  })
+    try {
+      // Get completed jobs from all queues
+      completedJobs = (
+        await Object.values(queues).reduce(async (acc, queue) => {
+          return Promise.resolve([ ...(await acc), ...(await queue.getCompleted()) ])
+        }, Promise.resolve([]))
+      )
 
-  queue.on('completed', (job, result) => {
-    sendToMainWindow('job succeeded', { job, result })
-  })
-
-  queue.on('failed', (job, err) => {
-    sendToMainWindow('job failed', { job, err })
-  })
-})
-
-ipc.on('get preferences', (event, data) => {
-  event.returnValue = loadSettings()
-})
-
-ipc.on('save preferences', (event, data) => {
-  prefs = validateSettings(data)
-  settings.setAll(prefs)
-})
-
-ipc.on('reset preferences', (event, data) => {
-  settings.setAll(defaults)
-  event.returnValue = defaults
-})
-
-ipc.on('get job', async (event, data) => {
-  const queueName = data.job.queue.name
-  try {
-    event.returnValue = await queues[queueName].getJob(data.job.id)
-  }
-  catch (err) {
-    dialog.showErrorBox('Failed to get job', err.toString())
-  }
-})
-
-ipc.on('abort job', async (event, data) => {
-  Object.values(queues[data.job.queue.name].childPool.retained).forEach((child) => {
-    child.send({__abortJobId__: data.job.id})
-  })
-})
-
-ipc.on('get finished jobs', async (event, data) => {
-  let completedJobs, failedJobs
-
-  try {
-    // Get completed jobs from all queues
-    completedJobs = (
-      await Object.values(queues).reduce(async (acc, queue) => {
-        return Promise.resolve([ ...(await acc), ...(await queue.getCompleted()) ])
-      }, Promise.resolve([]))
-    )
-
-      // Sort completed jobs from all queues by finished time (desc)
-      .sort((a, b) => parseInt(b.finishedOn) - parseInt(a.finishedOn))
-  }
-  catch (err) {
-    dialog.showErrorBox('Failed to get completed jobs', err.toString())
-  }
-
-  try {
-    // Get failed jobs from all queues
-    failedJobs = (
-      await Object.values(queues).reduce(async (acc, queue) => {
-        return [ ...(await acc), ...(await queue.getFailed()) ]
-      }, Promise.resolve([]))
-    )
-
-      // Sort failed jobs from all queues by finished time (desc)
-      .sort((a, b) => parseInt(b.finishedOn) - parseInt(a.finishedOn))
-  }
-  catch (err) {
-    dialog.showErrorBox('Failed to get failed jobs', err.toString())
-  }
-
-  const jobs = [ ... completedJobs, ...failedJobs ]
-    .sort((a, b) => parseInt(b.finishedOn) - parseInt(a.finishedOn))
-
-  event.sender.send(
-    'finished jobs',
-    {
-      jobs: jobs.slice(0, data.limit || (completedJobs.length + failedJobs.length))
+        // Sort completed jobs from all queues by finished time (desc)
+        .sort((a, b) => parseInt(b.finishedOn) - parseInt(a.finishedOn))
     }
-  )
-})
+    catch (err) {
+      dialog.showErrorBox('Failed to get completed jobs', err.toString())
+    }
 
-ipc.on('get pending jobs', async (event, data) => {
-  let waitingJobs, delayedJobs
-  try {
-    waitingJobs = (
-      await Object.values(queues).reduce(async (acc, queue) => {
-        return Promise.resolve([ ...(await acc), ...(await queue.getWaiting()) ])
-      }, Promise.resolve([]))
+    try {
+      // Get failed jobs from all queues
+      failedJobs = (
+        await Object.values(queues).reduce(async (acc, queue) => {
+          return [ ...(await acc), ...(await queue.getFailed()) ]
+        }, Promise.resolve([]))
+      )
+
+        // Sort failed jobs from all queues by finished time (desc)
+        .sort((a, b) => parseInt(b.finishedOn) - parseInt(a.finishedOn))
+    }
+    catch (err) {
+      dialog.showErrorBox('Failed to get failed jobs', err.toString())
+    }
+
+    const jobs = [ ... completedJobs, ...failedJobs ]
+      .sort((a, b) => parseInt(b.finishedOn) - parseInt(a.finishedOn))
+
+    event.sender.send(
+      'finished jobs',
+      {
+        jobs: jobs.slice(0, data.limit || (completedJobs.length + failedJobs.length))
+      }
     )
-      .sort((a, b) => parseInt(b.timestamp) - parseInt(a.timestamp))
-  }
-  catch (err) {
-    dialog.showErrorBox('Failed to get waiting jobs', err.toString())
-  }
-
-  try {
-    delayedJobs = (
-      await Object.values(queues).reduce(async (acc, queue) => {
-        return Promise.resolve([ ...(await acc), ...(await queue.getDelayed()) ])
-      }, Promise.resolve([]))
-    )
-      .sort((a, b) => parseInt(b.timestamp) - parseInt(a.timestamp))
-  }
-  catch (err) {
-    dialog.showErrorBox('Failed to get delayed jobs', err.toString())
-  }
-
-  const jobs = [ ...waitingJobs, ...delayedJobs]
-    .sort((a, b) => parseInt(b.timestamp) - parseInt(a.timestamp))
-
-  event.sender.send('pending jobs', { jobs: jobs.slice(0, data.limit || waitingJobs.length) })
-})
-
-ipc.on('get active jobs', async (event, data) => {
-  try {
-    const jobs = (
-      await Object.values(queues).reduce(async (acc, queue) => {
-        return Promise.resolve([ ...(await acc), ...(await queue.getActive()) ])
-      }, Promise.resolve([]))
-    )
-      .sort((a, b) => parseInt(a.timestamp) - parseInt(b.timestamp))
-
-    event.sender.send( 'active jobs', { jobs })
-  }
-  catch (err) {
-    dialog.showErrorBox('Failed to get active jobs', err.toString())
-  }
-})
-
-queues['metadata'].process('update metadata', 1, path.join(jobsPath, 'metadata.js'))
-ipc.on('update metadata', (event, data) => {
-  const { bucket, prefix, metadata } = data
-  queues['metadata'].add('update metadata', {
-    ...prefs.awsCredentials,
-    bucket,
-    prefix,
-    metadata
   })
-})
 
-queues['image processing'].process(
-  'resize still',
-  prefs.general.imageProcessingWorkers,
-  path.join(jobsPath, 'resize-still.js')
-)
+  ipc.on('get pending jobs', async (event, data) => {
+    let waitingJobs, delayedJobs
+    try {
+      waitingJobs = (
+        await Object.values(queues).reduce(async (acc, queue) => {
+          return Promise.resolve([ ...(await acc), ...(await queue.getWaiting()) ])
+        }, Promise.resolve([]))
+      )
+        .sort((a, b) => parseInt(b.timestamp) - parseInt(a.timestamp))
+    }
+    catch (err) {
+      dialog.showErrorBox('Failed to get waiting jobs', err.toString())
+    }
 
-ipc.on('resize stills', (event, data) => {
-  new Set(data.files).forEach((imagePath) => {
-    Object.keys(rasters).forEach((raster) => {
-      queues['image processing'].add('resize still', { imagePath, raster })
+    try {
+      delayedJobs = (
+        await Object.values(queues).reduce(async (acc, queue) => {
+          return Promise.resolve([ ...(await acc), ...(await queue.getDelayed()) ])
+        }, Promise.resolve([]))
+      )
+        .sort((a, b) => parseInt(b.timestamp) - parseInt(a.timestamp))
+    }
+    catch (err) {
+      dialog.showErrorBox('Failed to get delayed jobs', err.toString())
+    }
+
+    const jobs = [ ...waitingJobs, ...delayedJobs]
+      .sort((a, b) => parseInt(b.timestamp) - parseInt(a.timestamp))
+
+    event.sender.send('pending jobs', { jobs: jobs.slice(0, data.limit || waitingJobs.length) })
+  })
+
+  ipc.on('get active jobs', async (event, data) => {
+    try {
+      const jobs = (
+        await Object.values(queues).reduce(async (acc, queue) => {
+          return Promise.resolve([ ...(await acc), ...(await queue.getActive()) ])
+        }, Promise.resolve([]))
+      )
+        .sort((a, b) => parseInt(a.timestamp) - parseInt(b.timestamp))
+
+      event.sender.send( 'active jobs', { jobs })
+    }
+    catch (err) {
+      dialog.showErrorBox('Failed to get active jobs', err.toString())
+    }
+  })
+
+  queues['metadata'].process('update metadata', 1, path.join(jobsPath, 'metadata.js'))
+  ipc.on('update metadata', (event, data) => {
+    const { bucket, prefix, metadata } = data
+    queues['metadata'].add('update metadata', {
+      ...prefs.awsCredentials,
+      bucket,
+      prefix,
+      metadata
+    })
+  })
+
+  queues['image processing'].process(
+    'resize still',
+    prefs.general.imageProcessingWorkers,
+    path.join(jobsPath, 'resize-still.js')
+  )
+
+  ipc.on('resize stills', (event, data) => {
+    new Set(data.files).forEach((imagePath) => {
+      Object.keys(rasters).forEach((raster) => {
+        queues['image processing'].add('resize still', { imagePath, raster })
+      })
+    })
+  })
+
+  queues['video transcoding'].process(
+    'transcode',
+    prefs.general.videoTranscodingWorkers,
+    path.join(jobsPath, '/transcode.js')
+  )
+
+  ipc.on('transcode', (event, data) => {
+    const { files, formats, rasters, passes } = data
+    new Set(files).forEach((videoPath) => {
+      // Generate thumbnails
+      if (!formats) {
+        return queues['video transcoding'].add(
+          'transcode',
+          {
+            videoPath,
+            thumbnailFrequency: prefs.ffmpeg.thumbnailFrequency,
+            thumbnailHeight: prefs.ffmpeg.thumbnailHeight,
+            thumbnailStackSize: prefs.ffmpeg.thumbnailStackSize,
+            thumbnailStacksOnly: prefs.ffmpeg.thumbnailStacksOnly
+          }
+        )
+      }
+
+      // Transcode to mp3
+      if (formats.mp3 && !/-(a|b).mov$/.test(videoPath)) {
+        queues['video transcoding'].add('transcode', { videoPath, format: 'mp3' })
+      }
+
+      // Transcode to mp4
+      if (formats.mp4) {
+        Object.keys(rasters).forEach((raster) => {
+          if (rasters[raster])
+            queues['video transcoding'].add('transcode', { videoPath, format: 'mp4', raster, passes })
+        })
+      }
     })
   })
 })
-
-queues['video transcoding'].process(
-  'transcode',
-  prefs.general.videoTranscodingWorkers,
-  path.join(jobsPath, '/transcode.js')
-)
-
-ipc.on('transcode', (event, data) => {
-  const { files, formats, rasters, passes } = data
-  new Set(files).forEach((videoPath) => {
-    // Generate thumbnails
-    if (!formats) {
-      return queues['video transcoding'].add(
-        'transcode',
-        {
-          videoPath,
-          thumbnailFrequency: prefs.ffmpeg.thumbnailFrequency,
-          thumbnailHeight: prefs.ffmpeg.thumbnailHeight,
-          thumbnailStackSize: prefs.ffmpeg.thumbnailStackSize,
-          thumbnailStacksOnly: prefs.ffmpeg.thumbnailStacksOnly
-        }
-      )
-    }
-
-    // Transcode to mp3
-    if (formats.mp3 && !/-(a|b).mov$/.test(videoPath)) {
-      queues['video transcoding'].add('transcode', { videoPath, format: 'mp3' })
-    }
-
-    // Transcode to mp4
-    if (formats.mp4) {
-      Object.keys(rasters).forEach((raster) => {
-        if (rasters[raster])
-          queues['video transcoding'].add('transcode', { videoPath, format: 'mp4', raster, passes })
-      })
-    }
-  })
-})
-
-app.on('ready', initialize)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
